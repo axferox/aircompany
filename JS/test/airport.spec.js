93ee63b9235cca31c21a647882115b5b89bdf747
axferox <axferox@gmail.com>
2021-11-14 00:52:09 +0300

    fixed test

--- JS/test/airport.spec.js
+++ JS/test/airport.spec.js
@@ -25,7 +25,7 @@ describe('My Test', () => {
         new MilitaryPlane('B-52 Stratofortress', 1000, 20000, 80000, MilitaryType.BOMBER),
         new MilitaryPlane('F-15', 1500, 12000, 10000, MilitaryType.FIGHTER),
         new MilitaryPlane('F-22', 1550, 13000, 11000, MilitaryType.FIGHTER),
-        new MilitaryPlane('C-130 Hercules', 650, 5000, 110000, MilitaryType.TRANSPORT),
+        new MilitaryPlane('C-130 Hercules', 650, 5000, 110000, MilitaryType.TYPE_TRANSPORT),
         new experimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
         new experimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
     ];
